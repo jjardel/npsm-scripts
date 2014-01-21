78abd40e59525e12ae9c1da35a42688d6ac924aa
jjardel <johnjardel@gmail.com>
2014-01-20 20:27:54 -0600

    plotting combined results too

--- local/plotall.py
+++ local/plotall.py
@@ -8,13 +8,15 @@ fig = plt.figure()
 gallist = [ 'Fornax', 'Carina', 'Sextans', 'Sculptor', 'Draco' ]
 gallist.sort()
 
-plt.subplot( 2, 3, 1 ).axis( 'off' )
+gallist.insert( 0, 'Combined' )
 
-iplt = 2
+#plt.subplot( 2, 3, 1 ).axis( 'off' )
+
+iplt = 1
 for galname in gallist:
     plt.subplot( 2, 3, iplt )
     
-    if iplt == 2 or iplt == 4:
+    if iplt == 1 or iplt == 4:
         left = True
     else:
         left = False
@@ -39,4 +41,4 @@ plt.subplots_adjust( hspace = 0.2, wspace = 0. )
 fig.text( 0.02, 0.5, '$\\rho_{DM} \, (M_{\odot} \, \mathrm{pc}^{-3})$',
           rotation = 'vertical', fontsize = '16' )
 fig.text( 0.5, 0.02, '$r$ $\\mathrm{(pc)}$', fontsize = '16' )
-plt.savefig( 'all.ps' )
+plt.savefig( 'all_combined.ps' )
